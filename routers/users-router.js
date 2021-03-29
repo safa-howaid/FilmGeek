bfd957ce636c297d7a5d7b3e34a66941a868d2d1
Safa <safa.howaid@gmail.com>
2021-03-29 18:37:27 -0400

    add support for registration

--- routers/users-router.js
+++ routers/users-router.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const path = require('path');
 const database = require('../data/database');
+const User = require("../data/models/userModel")
 
 //Create the router
 let router = express.Router();
 
 router.get('/', (request, response) => {response.send("Users Search")})
 router.get('/:id', displayUser)
+router.post('/', [validateUserInput, createNewUser])
 
 function displayUser(request, response) {
     let id = request.params.id;
@@ -24,5 +26,51 @@ function displayUser(request, response) {
         .type('html')
         .render("../views/pages/user", {user: user, database: database});
 }
+
+function validateUserInput(request, response, next) {
+    const username = request.body.username;
+    const password = request.body.password;
+    const confirmPassword = request.body.confirmPassword;
+
+    // Checks if passwords match
+    if (password != confirmPassword) {
+        request.session.errorMessage = "The passwords you entered do not match."
+        return response.redirect("/register")
+    } 
+
+    // Checks if username exists
+    User.exists({ username: username }, function(err, result) {
+        if (err) {
+          response.send(err);
+        } else if (result) {
+            request.session.errorMessage = "The username you entered is already taken."
+            return response.redirect("/register")
+        }
+        next()
+    });
+}
+
+function createNewUser(request, response, next) {
+    const username = request.body.username;
+    const password = request.body.password;
+
+    let newUser = new User ({
+        "username": username,
+        "password": password,
+    })
+    console.log(newUser)
+
+    // Saves new user and returns response
+    newUser.save(function (err, result) {
+        if (err) return console.error(err);
+        request.session.loggedIn = true;
+        request.session.username = username;
+        return response.format({
+            "text/html": () => response.redirect("/profile"),
+            "application/json": () => {response.status(201).json(result)}
+        });
+    });
+}
+
 //Export the router object so we can access it in the base app
 module.exports = router;
