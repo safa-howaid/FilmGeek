0332cc6eaa7c5c606b5d163fded2cec387805c53
Safa <safa.howaid@gmail.com>
2021-03-29 18:38:13 -0400

    Add support for viewing single movie

--- routers/movies-router.js
+++ routers/movies-router.js
@@ -1,29 +1,60 @@
 const express = require('express');
-const path = require('path');
-const database = require('../data/database');
+const mongoose = require("mongoose");
+const ObjectId= require('mongoose').Types.ObjectId
+const Movie = require("../data/models/movieModel")
+const Person = require("../data/models/personModel")
+const Review = require("../data/models/reviewModel")
 
-//Create the router
+// Create the router
 let router = express.Router();
 
 router.get('/', displayMovieSearchPage)
+router.get('/:id', sendMovie)
 
-router.get('/:id', displayMovie)
+// Load movie from database when given movie id
+router.param("id", function(request, response, next, id) {
+    let oid;
+    console.log("Finding movie by ID: " + id);
 
-function displayMovie(request, response) {
-    let id = request.params.id;
-    let movie = database.getMovieById(id);
+	try{
+		oid = new ObjectId(id);
+	}catch(err){
+		console.log(err);
+		res.status(404).send("That movie does not exist.");
+		return;
+	}
 
-    // If movie is not found, send error
-    if (movie == undefined) {
-        response.status(404)
-            .send("Page not found")
-        return
-    }
+    Movie.findById(oid)
+    .populate("actors", "name")
+    .populate("directors", "name")
+    .populate("writers", "name")
+    .populate("reviews")
+	.exec(function(err, result){
+		if(err){
+			console.log(err);
+			response.status(500).send("Error reading movie data.");
+			return;
+		}
+		
+		if(!result){
+			response.status(404).send("That movie does not exist.");
+			return;
+		}
+		console.log("Result:");
+		console.log(result);
+		response.movie = result;
+		next();
+	});
+})
 
-    // Send rendered movie page
-    response.status(200)
-        .type('html')
-        .render("../views/pages/movie", {movie: movie, database: database});
+// Send a single movie page/object
+function sendMovie(request, response) {
+    // Send rendered movie page or movie json data
+    response.format({
+		"text/html": () => {response.render("../views/pages/movie", {movie: response.movie})},
+		"application/json": () => {response.status(200).json(response.movie)}
+	});
+	next();
 }
 
 function displayMovieSearchPage(request, response) {
